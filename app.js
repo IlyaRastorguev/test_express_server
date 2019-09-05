9d994c8d43a875947d7f38dd7978756ab7f38345
Расторгуев Илья Анатольевич <Rastorguev.I.Ana@sberbank.ru>
2019-09-05 20:58:18 +0300

    Add new method "kittens", change some API

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var flutter = require('./routes/flutterRoute');
 
 var app = express();
--- routes/flutterRoute.js
+++ routes/flutterRoute.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-var json = require('../public/static/test');
+var json = require('../public/static/json/test');
+var kittens = require('../public/static/json/cards');
 
 
-router.get('/', function(req, res, next) {
+router.get('/start', function(req, res, next) {
     res.send(json);
+});
+
+router.get('/kittens', function(req, res, next) {
+    res.send(kittens);
 });
 
 module.exports = router;
